6095a3192f0736bd0a9c17f91cb4c8f0987a3d98
John Reitano <jreitano@gmail.com>
2016-05-13 11:57:35 -0700

    set bootnodes for UR network

--- cmd/utils/bootnodes.go
+++ cmd/utils/bootnodes.go
@@ -22,20 +22,14 @@ import "github.com/urcapital/go-ur/p2p/discover"
 // the Frontier network.
 var FrontierBootNodes = []*discover.Node{
 	// ETH/DEV Go Bootnodes
-	discover.MustParseNode("enode://a979fb575495b8d6db44f750317d0f4622bf4c2aa3365d6af7c284339968eef29b69ad0dce72a4d8db5ebb4968de0e3bec910127f134779fbcb0cb6d3331163c@52.16.188.185:19595"), // IE
-	discover.MustParseNode("enode://de471bccee3d042261d52e9bff31458daecc406142b401d4cd848f677479f73104b9fdeb090af9583d3391b7f10cb2ba9e26865dd5fca4fcdc0fb1e3b723c786@54.94.239.50:19595"),  // BR
-	discover.MustParseNode("enode://1118980bf48b0a3640bdba04e0fe78b1add18e1cd99bf22d53daac1fd9972ad650df52176e7c7d89d1114cfef2bc23a2959aa54998a46afcf7d91809f0855082@52.74.57.123:19595"),  // SG
-
-	// ETH/DEV Cpp Bootnodes
-	discover.MustParseNode("enode://979b7fa28feeb35a4741660a16076f1943202cb72b6af70d327f053e248bab9ba81760f39d0701ef1d8f89cc1fbd2cacba0710a12cd5314d5e0c9021aa3637f9@5.1.83.226:19595"),
+	discover.MustParseNode("enode://fcf730cf678d6296ffa75a1b2c06aa07d9558788762d0bbefbc209ccbfb4e840f7dcfc2f7a188eb2e65056d989de3722df3fc4df286eb3690d4586992c1c6d82@45.55.7.66:19595"), // bootnode1
+	discover.MustParseNode("enode://d846b3c0445b7a91cfeb56fbeaece55ca9e559a6e5810cc41c54e2b88790fa7a24444508f16eb983630da1367ab73a6db1b705cc36134d9e61a2df070284d3f4@45.55.7.75:19595"), // bootnode2
 }
 
 // TestNetBootNodes are the enode URLs of the P2P bootstrap nodes running on the
 // Morden test network.
 var TestNetBootNodes = []*discover.Node{
 	// ETH/DEV Go Bootnodes
-	discover.MustParseNode("enode://e4533109cc9bd7604e4ff6c095f7a1d807e15b38e9bfeb05d3b7c423ba86af0a9e89abbf40bd9dde4250fef114cd09270fa4e224cbeef8b7bf05a51e8260d6b8@94.242.229.4:29595"),
-	discover.MustParseNode("enode://8c336ee6f03e99613ad21274f269479bf4413fb294d697ef15ab897598afb931f56beb8e97af530aee20ce2bcba5776f4a312bc168545de4d43736992c814592@94.242.229.203:29595"),
-
-	// ETH/DEV Cpp Bootnodes
+	discover.MustParseNode("enode://d1778b1d1e3c2e7053310b9749ceffc28ea8d5fd0f066f1ffd8b227e9be4f9a0dcb5340ca16003c3c54b664bd12f146a510b3fc58c5140303e333e836f0c4bb6@138.68.63.204:19595"), // testbootnode1
+	discover.MustParseNode("enode://ee5e03c62179f6b3269ace0b9cee23aa29724251027de4b507080f2f793e0119e5e5838ddce47e3c5b5733fb08a65cb60bf43aeb4a6493c8a74eb20bf207bc38@138.68.56.175:19595"), // testbootnode2
 }
