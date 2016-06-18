12aa64ee081159f35c546932d635b1c4e9f7c1ea
John Reitano <jreitano@gmail.com>
2016-06-18 15:01:32 -0700

    add items needed for testnet

--- eth/backend.go
+++ eth/backend.go
@@ -76,8 +76,7 @@ var (
 
 	defaultTestNetBootNodes = []*discover.Node{
 		// TODO-UR: add testnet nodes
-		//discover.MustParseNode("enode://e4533109cc9bd7604e4ff6c095f7a1d807e15b38e9bfeb05d3b7c423ba86af0a9e89abbf40bd9dde4250fef114cd09270fa4e224cbeef8b7bf05a51e8260d6b8@94.242.229.4:40404"),
-		//discover.MustParseNode("enode://8c336ee6f03e99613ad21274f269479bf4413fb294d697ef15ab897598afb931f56beb8e97af530aee20ce2bcba5776f4a312bc168545de4d43736992c814592@94.242.229.203:30303"),
+		discover.MustParseNode("enode://c39ec5b1d816b0068c70fefe7f8152d7bc35127cc78aa37be5ce928de2a4ef0a1ec98445fedd2efc5f4eef75d63d2a5b96a5d69e3a8bcdeaf140c6d359e7b1a9@45.79.198.10:19596"),
 	}
 	staticNodes  = "static-nodes.json"  // Path within <datadir> to search for the static node list
 	trustedNodes = "trusted-nodes.json" // Path within <datadir> to search for the trusted node list
