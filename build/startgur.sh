1329fa063b54d58b94a7f82049531716db931141
John Reitano <jreitano@gmail.com>
2016-07-11 18:17:25 -0700

    update timestamp, coinbase, and initial difficulty of genesis block also, update seed node addresses

--- build/startgur.sh
+++ build/startgur.sh
@@ -3,12 +3,5 @@
 killall gur
 sleep 3
 cd ~/go-ur
-
-if [ $1 = "staging" ]; then
-  extra_params="--port 19596 --genesis build/testnet/testnet_genesis.json"
-else
-  extra_params=""
-fi
-echo "starting gur with extra params: $extra_params"
-nohup build/bin/gur $extra_params </dev/null >>/tmp/gur.log 2>&1 &
+nohup build/bin/gur </dev/null >>/tmp/gur.log 2>&1 &
 sleep 3
