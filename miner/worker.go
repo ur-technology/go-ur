75b28abe32b140f567b9642e45e636e95fee0d5e
Hélio Rosa <spinny666@gmail.com>
2017-01-14 02:34:59 +0000

    more precise accounting for commited transactions

--- miner/worker.go
+++ miner/worker.go
@@ -508,17 +508,6 @@ func (self *worker) commitNewWork() {
 	}
 	txs := types.NewTransactionsByPriceAndNonce(self.eth.TxPool().Pending())
 	commitedTxs := work.commitTransactions(self.mux, txs, self.gasPrice, self.chain)
-	totalGas := big.NewInt(0)
-	nChecked := 0
-	for _, tx := range commitedTxs {
-		newTotalGas := new(big.Int).Add(totalGas, tx.Gas())
-		if newTotalGas.Cmp(header.GasLimit) > 0 {
-			break
-		}
-		totalGas = newTotalGas
-		nChecked++
-	}
-	commitedTxs = commitedTxs[:nChecked]
 
 	self.eth.TxPool().RemoveBatch(work.lowGasTxs)
 	self.eth.TxPool().RemoveBatch(work.failedTxs)
@@ -642,8 +631,8 @@ func (env *Work) commitTransactions(mux *event.TypeMux, txs *types.TransactionsB
 			coalescedLogs = append(coalescedLogs, logs...)
 			env.tcount++
 			txs.Shift()
+			r = append(r, tx)
 		}
-		r = append(r, tx)
 	}
 
 	if len(coalescedLogs) > 0 || env.tcount > 0 {
